d639f66d9ab57130bf5aab4f61598d1d6e86d0fe
kbaba1001 <kbaba1001@users.noreply.github.com>
2017-04-27 15:47:36 +0900

    add dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' } do
+    authenticated :user do
+      root :to => 'dashboards#show', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "devise/sessions#new"
 end
